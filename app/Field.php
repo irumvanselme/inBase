7c1327be65fc1503e07d668bffb2b9ad2b11fa58
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-27 18:20:16 +0200

    Fix Field model

--- app/Field.php
+++ app/Field.php
@@ -4,28 +4,11 @@ namespace App;
 
 use Jenssegers\Mongodb\Eloquent\Model;
 
-/**
- * @property mixed required
- * @property mixed type
- * @property mixed minimum
- * @property mixed maximum
- * @property mixed data_in
- * @property mixed data_not_in
- */
-
 class Field extends Model
 {
     protected $fillable = [
         "name",
-        "type",
-        "required",
-        "default",
-        "minimum",
-        "maximum",
-        "data_in",
-        "unique",
-        "data_not_in",
-        "slug"
+        "validations"
     ];
 
     public function table(){
@@ -34,19 +17,6 @@ class Field extends Model
 
     public function data(){
         return $this->hasMany(Data::class);
-    }
-
-    public function validations(){
-        $validations = [];
-        $this->required === true ? $validations[] = "required" : false;
-        $validations[] =  $this->type;
-        $validations[] =  "min:".$this->minimum;
-        $validations[] =  "max:".$this->maximum;
-
-        isset($this->data_in) && sizeof($this->data_in) ? $validations[] = "in:".implode(",",$this->data_in) : false;
-        isset($this->data_not_in) && sizeof($this->data_not_in) > 0 ? $validations[] = "not_in:".implode(",",$this->data_not_in) : false;
-
-        return $validations;
     }
 
     public $timestamps = false;
