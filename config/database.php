fbe1c2f776598d9361bae17a465a7ff0d105bd57
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-29 08:09:12 +0200

    Add an online database

--- config/database.php
+++ config/database.php
@@ -37,12 +37,16 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'dsn' => 'mongodb+srv://anselme:123@cluster0.gu14c.mongodb.net/inBase?retryWrites=true&w=majority',
+//            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'in_tabular'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+//            'database' => env('DB_DATABASE', 'homestead'),
+//            'username' => env('DB_USERNAME', 'homestead'),
+//            'password' => env('DB_PASSWORD', 'secret'),
             'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+
                 'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
             ],
         ]
